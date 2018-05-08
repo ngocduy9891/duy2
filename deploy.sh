5c6ad9b8e7fc1ef3b88a20a724e82e23bf948369
ngoc duy tran <36229133+ngocduy9891@users.noreply.github.com>
2018-05-08 14:14:49 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 SHA1=$1
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
-EB_BUCKET= # Elastic Beanstalkのバージョンファイルが入るS3のバケット名
+EB_BUCKET= ngocduy-deploy-bucket
 
 aws ecr get-login | bash
 
-docker build -t {AWS ID}.dkr.ecr.us-east-1.amazonaws.com/{リポジトリ名}:$SHA1 .
-docker push {AWS ID}.dkr.ecr.us-east-1.amazonaws.com/{リポジトリ名}:$SHA1
+docker build -t 012881927014.dkr.ecr.ap-southeast-1.amazonaws.com/docker:$SHA1 .
+docker push 012881927014.dkr.ecr.ap-southeast-1.amazonaws.com/docker:$SHA1
 
 sed "s/<tag>/$SHA1/" < Dockerrun.aws.json > $DOCKERRUN_FILE
 
